d1ae856c032c366193c9bb4bce8c8181179370ef
am-hasib <amhasiv2014@gmail.com>
2024-02-12 18:49:22 +0600

    Inital Stage

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./about-us.html",
+    "./contact.html",
+    "./news.html",
+    "./services.html",
+    "./src/**/*.{html,js}",
+    "./styles/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
